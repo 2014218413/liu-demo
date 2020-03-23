aca3208b8ccde6009826b0842bf74dd2fd314dbb
2014218413@qq.com <2014218413@qq.com>
2020-03-23 13:24:14 +0800

    20200323

---
+++ liu_project/src/main/java/liu_project/tables/newShop/MoneySuccess.java
@@ -0,0 +1,44 @@
+package liu_project.tables.newShop;
+
+/**
+ * @author 忌冷忌热的怕热不怕冷的吃不胖少年
+ * 回首向来萧瑟处，也无风雨也无晴。
+ **/
+public class MoneySuccess {
+    private double zhifu;
+    private String shouhuoren;
+    private String lianxidianhua;
+    private String shouhuodizhi;
+
+    public double getZhifu() {
+        return zhifu;
+    }
+
+    public void setZhifu(double zhifu) {
+        this.zhifu = zhifu;
+    }
+
+    public String getShouhuoren() {
+        return shouhuoren;
+    }
+
+    public void setShouhuoren(String shouhuoren) {
+        this.shouhuoren = shouhuoren;
+    }
+
+    public String getLianxidianhua() {
+        return lianxidianhua;
+    }
+
+    public void setLianxidianhua(String lianxidianhua) {
+        this.lianxidianhua = lianxidianhua;
+    }
+
+    public String getShouhuodizhi() {
+        return shouhuodizhi;
+    }
+
+    public void setShouhuodizhi(String shouhuodizhi) {
+        this.shouhuodizhi = shouhuodizhi;
+    }
+}
